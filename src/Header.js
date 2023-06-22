c7e030e7773048113347230bb302f3ff4c696ec8
Antra Phukan <antraphukan.dib@gmail.com>
2023-06-22 18:00:44 +0530

    modified components file

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+//import profile from "./Images/profile.png";
 
 export default function Header() {
     return (
         <header>
             <nav>
+
+
                 <ul className="nav-bar">
+                    <img src="./Images/profile.png" />
                     <li>About Me</li>
                     <li>Education</li>
                     <li>Projects</li>
